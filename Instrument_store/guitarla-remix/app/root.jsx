24b97252bb1d5723fee83ec5230e0c3e9f4b6691
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-12-09 21:28:56 -0500

    Se crea proyecto remis de guitarLA.

--- Instrument_store/guitarla-remix/app/root.jsx
+++ Instrument_store/guitarla-remix/app/root.jsx
@@ -1,33 +1,20 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
+export default function App(){
+    return (
+        <Document>
+            <h1>Hola Mundo</h1>
+        </Document>
+    );
+}
 
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-
-export const links = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
-
-export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+function Document ({children}){
+    return (
+        <html lang="es">
+            <head>
+                <title>GuitarLA - Remix</title>
+            </head>
+            <body>
+                {children}
+            </body>
+        </html>
+    );
 }
