2366e5e0cbfe622d642023775ee8c60097eee9ff
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-12-12 20:25:57 -0500

    Se agrega la api de guitarras en la pagina tiendas.

---
+++ Instrument_store/guitarla-remix/app/models/guitarras.server.js
@@ -0,0 +1,6 @@
+export async function getGuitarras() {
+    const response = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`);
+    const result = await response.json();
+
+    return result;
+}   
