3ea0ecb79d6c8f686edfe3bb6f5026ac04530670
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-12-15 16:42:57 -0500

    Se agregan los estilos para la visualización de los blogs.

--- Instrument_store/guitarla-remix/app/routes/posts.$url.jsx
+++ Instrument_store/guitarla-remix/app/routes/posts.$url.jsx
@@ -1,6 +1,33 @@
 import { useLoaderData } from '@remix-run/react'
 import { getPost } from '~/models/posts.server';
-import { formatearFecha } from '~/utils/helpers'
+import { formatearFecha } from '~/utils/helpers';
+import styles from '~/styles/blog.css';
+
+export function meta({data}){
+    /** Sí no hay data**/
+    if(!data){
+        return {
+            title: 'Blog no encontrada',
+            description: `GuitarLA - venta de guitarras, guitarra no encontrada`
+        }
+    }
+
+    return [
+        {
+            title: `GuitarLA - ${data.data[0].attributes.titulo}`,
+            description: `GuitarLA - venta de guitarras ${data.data[0].attributes.titulo}`
+        }
+    ]
+}
+
+export function links(){
+    return [
+        {
+            rel: 'stylesheet',
+            href: styles
+        }
+    ]
+}
 
 export async function loader({params}){
     
@@ -17,10 +44,12 @@ export async function loader({params}){
 }
 
 export default function Post() {
-    const post = useLoaderData()
+
+    const post = useLoaderData();
+
     const { titulo, contenido, imagen, publishedAt } = post?.data[0]?.attributes
     return (
-      <article className='  '>
+      <article className='contenedor post mt-3'>
           <img className="imagen" src={imagen?.data?.attributes?.url} alt={`imagen blog ${titulo}`} />
               <div className="contenido">
                   <h3>{titulo}</h3>
